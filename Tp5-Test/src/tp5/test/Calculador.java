d8bbca50c27e184664fb68ce7ce7daa606d2d1e6
Gastoncito93 <gastonoscarsosa@gmail.com>
2025-06-02 19:44:29 -0300

    Mails Agregados

--- Tp5-Test/src/tp5/test/Calculador.java
+++ Tp5-Test/src/tp5/test/Calculador.java
@@ -1,3 +1,10 @@
+//Nombre Integrantes:
+//Muñoz Yaciel - yacielzombers@gmail.com
+//Sosa Gaston - gastonoscarsosa@gmail.com
+//Copola Valentino - 
+//Valentin Bergoglio - 
+//Fernando - orfel@merlo-sl.com,ar 
+//Alberto Pereyra - 
 package tp5.test;
 
 public class Calculador {
